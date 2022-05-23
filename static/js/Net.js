d1f54efcb615bdd7827b5b17e524201e4bb1a99b
lythx <lythxxx@gmail.com>
2022-05-23 20:41:33 +0200

    rggfdfdg

--- static/js/Net.js
+++ static/js/Net.js
@@ -1,7 +1,16 @@
 class Net {
-    
-    login() {
-       console.log('sfdfsdsfd') 
-    }
 
+    async login() {
+        const login = document.getElementById('txt').value
+        const body = JSON.stringify({ login })
+        const response = await fetch('/addlogin', {
+            method: 'POST',
+            body,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+    }
 }
