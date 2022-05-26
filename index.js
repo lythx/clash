7095e7bcacf6b430821b3f6c07dd4c99f8f23512
lythx <lythxxx@gmail.com>
2022-05-26 22:51:14 +0200

    timer

--- index.js
+++ index.js
@@ -9,6 +9,44 @@ const players = ['janiarz']
 let gameRunning = false
 let status = 'waiting'
 
+class Timer {
+    moveTime
+    stop = true
+    startTimer = () => {
+        if (!this.stop)
+            return
+        this.stop = false
+        this.moveTime = 30
+        let lastDate = Date.now()
+        const render = () => {
+            if (this.stop)
+                return
+            if (this.moveTime === -1) {
+                handleTimeEnd()
+                return
+            }
+            setImmediate(render)
+            if (Date.now() - lastDate < 1000)
+                return
+            lastDate = Date.now()
+            this.moveTime--
+        }
+        setImmediate(render)
+    }
+
+    resetTimer = () => {
+        this.moveTime = 30
+    }
+
+    stopTimer = () => {
+        this.stop = true
+    }
+
+    getMoveTime = () => {
+        return this.moveTime
+    }
+}
+
 app.post('/addlogin', (req, res) => {
     if (players.length > 1) {
         res.send(JSON.stringify({ status: 'TOO MANY LOGGED IN' }))
