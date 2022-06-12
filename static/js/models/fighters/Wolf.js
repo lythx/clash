f42f11eab3db0efb9daf3cc565da18bdcc04759f
lythx <lythxxx@gmail.com>
2022-06-12 14:22:10 +0200

    wolf

--- static/js/models/fighters/Wolf.js
+++ static/js/models/fighters/Wolf.js
@@ -33,15 +33,15 @@ class Wolf extends Fighter {
     async handleAttack(target, dmg) {
         // DMG bedzie taki jak atak tylko w przypadku głownego targetu, a tylko tam tworzymy eksplozje (żeby była tylko jedna)
         if (dmg === this.attack) {
-            Game.scene.add(new Explosion(4, target.position))
+            Game.scene.add(new Explosion(3, { x: target.position.x, y: target.position.y + 5, z: target.position.z }))
             // Wybuch przy broni strzelającego
             // Obliczenie kątu pod którym jest obrócony ten co strzela
             let angle = Math.atan2(target.position.z - this.position.z, -(target.position.x - this.position.x)) + (2 * Math.PI)
             if (angle >= 2 * Math.PI) //układ współrzędnych jest tu jakoś dziwnie ustawiony, więc trzeba tak zrobić
                 angle -= 2 * Math.PI
-            const distance = 15 // Wybuch musi być troche z przodu, taki dystans daje dobry efekt
+            const distance = 10 // Wybuch musi być troche z przodu, taki dystans daje dobry efekt
             // Tu do x dodane 2 bo bron jest troche po prawej stronie
-            Game.scene.add(new Explosion(4, { x: (this.position.x - distance * Math.cos(angle)) + 2, y: this.position.y + 8, z: this.position.z + distance * Math.sin(angle) }))
+            Game.scene.add(new Explosion(2, { x: (this.position.x - distance * Math.cos(angle)) + 1, y: this.position.y + 5, z: this.position.z + distance * Math.sin(angle) }))
         }
         this.attackAnimation()
         // Opóźnienie dmga żeby zgrał sie z animacją ataku
